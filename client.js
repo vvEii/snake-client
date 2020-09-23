b21021fa4b6773333dcbcfcc04bc1f1aa094ab84
shangguanwei <coopershang125@gmail.com>
2020-09-23 20:21:22 +0000

    snake initials

--- client.js
+++ client.js
@@ -15,6 +15,11 @@ const connect = function () {
     console.log(data);
   });
 
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server');
+    conn.write('Name: SGW');
+  });
+
   return conn;
 };
 
